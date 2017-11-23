47920f4c9d46ae2eaf8f57873c04595b1f513714
Dan Blondell <danblondell@gmail.com>
2017-11-23 17:39:36 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,9 +4,9 @@ class Pirate
   @all = []
 
   def initialize(:name, :weight, :height)
-    name
-    weight
-    height
+    @name = name
+    @weight = weight
+    @height = height
     @@all << self
   end
 
